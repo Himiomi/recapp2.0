5dcec817625f3b75de996169a38a9f39141ef861
Hikachhu <46454743+Hikachhu@users.noreply.github.com>
2021-12-29 23:05:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -43,17 +43,12 @@ const addBtn = document.querySelector('.add-button');
 
 window.addEventListener('beforeinstallprompt', (e) => {
 	
-  alert("hello 2");
   // Prevent Chrome 67 and earlier from automatically showing the prompt
   e.preventDefault();
   // Stash the event so it can be triggered later.
   deferredPrompt = e;
-  // Update UI to notify the user they can add to home screen
-  addBtn.style.display = 'block';
 
   addBtn.addEventListener('click', () => {
-    // hide our user interface that shows our A2HS button
-    addBtn.style.display = 'none';
     // Show the prompt
     deferredPrompt.prompt();
     // Wait for the user to respond to the prompt
