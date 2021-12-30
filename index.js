a2ea2d3edf8c0f7b5565fdeb043a76dee8bec933
Hikachhu <46454743+Hikachhu@users.noreply.github.com>
2021-12-30 13:50:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -33,12 +33,13 @@ window.addEventListener('beforeinstallprompt', (e) => {
   deferredPrompt = e;
   // Update UI to notify the user they can add to home screen
   addBtn.style.display = 'block';
-
+  alert("PASSAGE");
   addBtn.addEventListener('click', () => {
     // hide our user interface that shows our A2HS button
     addBtn.style.display = 'none';
     // Show the prompt
     deferredPrompt.prompt();
+    alert(Version);
     // Wait for the user to respond to the prompt
     deferredPrompt.userChoice.then((choiceResult) => {
       if (choiceResult.outcome === 'accepted') {
