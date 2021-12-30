aeeb4153b481dec1249d56ffbce4484b1d6bab4e
Hikachhu <46454743+Hikachhu@users.noreply.github.com>
2021-12-30 14:55:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -46,16 +46,3 @@ window.addEventListener('beforeinstallprompt', (e) => {
     });
   });
 });
-document.getElementById("add-button").addEventListener('click', () => {
-  // Show the prompt
-  deferredPrompt.prompt();
-  // Wait for the user to respond to the prompt
-  deferredPrompt.userChoice.then((choiceResult) => {
-    if (choiceResult.outcome === 'accepted') {
-      console.log('User accepted the A2HS prompt');
-    } else {
-      console.log('User dismissed the A2HS prompt');
-    }
-    deferredPrompt = null;
-  });
-});
