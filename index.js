b476be15a4607cf2e1d4d77a019e87f462c9749e
Hikachhu <florian.morin@gmail.com>
2021-12-31 14:49:59 +0100

    Ajout notification

--- index.js
+++ index.js
@@ -46,3 +46,66 @@ window.addEventListener('beforeinstallprompt', (e) => {
     });
   });
 });
+
+const notificationButton = document.getElementById("enableNotifications");
+let swRegistration = null;
+
+
+function initializeApp() {
+  if ("serviceWorker" in navigator && "PushManager" in window) {
+    console.log("Service Worker and Push is supported");
+
+    //Register the service worker
+    navigator.serviceWorker
+      .register("/recapp2.0/sw.js")
+      .then(swReg => {
+        console.log("Service Worker is registered", swReg);
+
+        swRegistration = swReg;
+        initializeUi();
+      })
+      .catch(error => {
+        console.error("Service Worker Error", error);
+      });
+  } else {
+    console.warn("Push messaging is not supported");
+    notificationButton.textContent = "Push Not Supported";
+  }
+}
+
+
+function initializeUi() {
+  notificationButton.addEventListener("click", () => {
+    displayNotification();
+  });
+}
+
+function displayNotification() {
+  if (window.Notification && Notification.permission === "granted") {
+    notification();
+  }
+  // If the user hasn't told if he wants to be notified or not
+  // Note: because of Chrome, we are not sure the permission property
+  // is set, therefore it's unsafe to check for the "default" value.
+  else if (window.Notification && Notification.permission !== "denied") {
+    Notification.requestPermission(status => {
+      if (status === "granted") {
+        notification();
+      } else {
+        alert("You denied or dismissed permissions to notifications.");
+      }
+    });
+  } else {
+    // If the user refuses to get notified
+    alert(
+      "You denied permissions to notifications. Please go to your browser or phone setting to allow notifications."
+    );
+  }
+}
+
+function notification() {
+  const options = {
+    body: "Testing Our Notification",
+  };
+  swRegistration.showNotification("PWA Notification!", options);
+}
